2684b80df5dd271815803a860bef18fab4da6b5d
EgorSamotyuk <e2000samotyuk69@gmail.com>
2020-01-26 22:50:25 +0200

    Delete whitenoise

--- YouTubeApi/settings.py
+++ YouTubeApi/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,9 +128,3 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'e2000samotyuk69@gmail.com'
-# EMAIL_HOST_PASSWORD = '6092000a'
--- YouTubeApi/wsgi.py
+++ YouTubeApi/wsgi.py
@@ -6,13 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
-
+from django.core.wsgi import get_wsgi_application 
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 import os
-
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YouTubeApi.settings')
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = StaticFilesHandler(get_wsgi_application())
