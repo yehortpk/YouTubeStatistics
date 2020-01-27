b06ab337fec83a444daaddb20905a4c3e735071c
EgorSamotyuk <e2000samotyuk69@gmail.com>
2020-01-27 11:06:41 +0200

    Change api connect

--- AccountInfo/api_methods.py
+++ AccountInfo/api_methods.py
@@ -18,7 +18,7 @@ class ApiMethods:
         flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
             client_secrets_file, scopes)
         if request.session.get('credentials') == None:
-            credentials = flow.run_local_server()
+            credentials = flow.run_local_server(open_browser=True)
             
             request.session['credentials'] = {
                 'token': credentials.token,
